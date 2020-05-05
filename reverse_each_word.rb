d804fafb8c7f614c240652e52ec9e73782bd5fa4
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-05-05 21:23:40 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,33 @@
+def reverse_each_word(string)
+  
+ first_array = string.split(" ")
+ reverse_array = []
+ 
+ first_array.each do |item|
+  reverse_string = ""
+  i = item.length - 1
+  while i >= 0
+    reverse_string += item[i]
+    i -= 1
+  end
+  reverse_array.push(reverse_string)
+  end
+  final_sentence = reverse_array.join(" ")
+end
+
+def reverse_each_word(string)
+  
+ first_array = string.split(" ")
+ reverse_array = []
+ 
+ first_array.collect do |item|
+  reverse_string = ""
+  i = item.length - 1
+  while i >= 0
+    reverse_string += item[i]
+    i -= 1
+  end
+  reverse_array.push(reverse_string)
+  end
+  final_sentence = reverse_array.join(" ")
+end
